2695713d215431f316d94d0f90d4fc6d1fd28163
Özgür Coban <infuser-mitosis0q@icloud.com>
2024-08-13 20:56:00 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      Populate me with Sanity Content
+    </main>
+  );
+}
