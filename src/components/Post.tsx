9f58908c0f6eea9937d8d9a5e35432a61cb992b8
Özgür Coban <infuser-mitosis0q@icloud.com>
2024-08-22 23:09:34 +0200

    feat: add .prettierrc with Tailwind CSS plugin and single quote configuration

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -9,11 +9,11 @@ export default function POST({ post }: { post: POST_QUERYResult }) {
   const { title, mainImage, body } = post || {};
 
   return (
-    <main className="container mx-auto prose prose-lg p4">
+    <main className="p4 container prose prose-lg mx-auto">
       {title ? <h1>{title}</h1> : null}
       {mainImage?.asset?._ref ? (
         <Image
-          className="float-left m-0 w-1/3 mr-4 rounded-lg"
+          className="float-left m-0 mr-4 w-1/3 rounded-lg"
           src={urlFor(mainImage?.asset._ref).width(300).height(300).url()}
           width={300}
           height={300}
