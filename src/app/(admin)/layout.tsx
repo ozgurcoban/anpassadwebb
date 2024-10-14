04582577fdb8838d31d00a6a9dd27519bb2923ae
MARSIF123 <maryamw561@gmail.com>
2024-10-14 15:22:51 -0400

    Fix: resolve visual editing issue

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,6 +1,4 @@
 import './globals.css';
-import { VisualEditing } from 'next-sanity';
-import { draftMode } from 'next/headers';
 
 export const metadata = {
   title: 'ÖzByte Studio',
@@ -11,19 +9,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en">
       <body>
-        {draftMode().isEnabled && (
-          <a
-            className="fixed bottom-0 right-0 m-4 bg-blue-500 p-4 text-white"
-            href="/api/draft-mode/disable"
-          >
-            Disable preview mode
-          </a>
-        )}
         {children}
-        {draftMode().isEnabled && <VisualEditing />}
       </body>
     </html>
   );
--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer';
 import Modal from '@/components/Modal';
 const openSans = Open_Sans({ subsets: ['latin'] });
+import { VisualEditing } from "next-sanity";
+import { draftMode } from "next/headers";
 
 export default function RootLayout({
   children,
@@ -14,12 +16,22 @@ export default function RootLayout({
       <body
         className={`${openSans.className} flex min-h-svh flex-col overflow-y-scroll`}
       >
+         {draftMode().isEnabled && (
+          <a
+            className="fixed right-0 bottom-0 bg-blue-500 text-white p-4 m-4"
+            href="/api/draft-mode/disable"
+          >
+            Disable preview mode
+          </a>
+        )}
         <Providers>
           <Navbar />
           <main className="container mt-8 flex-grow">{children}</main>
           <Footer />
           <Modal />
         </Providers>
+        {draftMode().isEnabled && <VisualEditing />}
+
       </body>
     </html>
   );
